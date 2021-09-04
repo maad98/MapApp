0c1cbe5f17d48ab465c7a77e5ec288700794eb47
Maad Abduljaleel <maad1998@hotmail.co.uk>
2021-09-04 14:18:22 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./index.css";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import mapStyles from "./mapstyles";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import branches from "./branches.json";
+require("dotenv").config();
 
-function App() {
+const libraries = ["places"];
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const center = {
+  lat: 51.2538,
+  lng: -85.3232,
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const API_KEY = process.env.REACT_APP_GOOGLE_KEY;
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+    libraries,
+  });
+
+  const [selected, setSelected] = React.useState(null);
+
+  const mapRef = React.useRef();
+
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search panTo={panTo} />
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={7}
+        center={center}
+        options={options}
+        onLoad={onMapLoad}
+      >
+        {" "}
+        {branches.map((branch) => (
+          <Marker
+            key={Math.random().toString(36).substr(2, 9)}
+            position={{ lat: branch.lat, lng: branch.lng }}
+            onClick={() => {
+              setSelected(branch);
+              console.log(branch);
+            }}
+          />
+        ))}
+        {selected && (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h3>
+                <strong>{selected.name}</strong>
+              </h3>
+              <h3>
+                Website:{" "}
+                <a href={selected.url} target="_blank" rel="noreferrer">
+                  {selected.url}
+                </a>
+              </h3>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
     </div>
   );
 }
 
-export default App;
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 51.2538, lng: () => -85.3232 },
+      radius: 200 * 1000,
+    },
+  });
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch (err) {
+            console.log(err);
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter an address"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ description }) => (
+                <ComboboxOption
+                  key={Math.random().toString(36).substr(2, 9)}
+                  value={description}
+                />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
