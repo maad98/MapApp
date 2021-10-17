542d2925e238a520335e85eb6a47ddc4bb544a65
Maad Abduljaleel <maad1998@hotmail.co.uk>
2021-10-16 21:03:46 -0400

    Minor edits
    Edited out the styling as well as some other attributes to embed it.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
-//import Text from "react-text";
-import logo from "./imgs/logo.webp";
 
 import {
   GoogleMap,
@@ -10,7 +8,6 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import mapStyles from "./mapstyles";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
@@ -29,10 +26,10 @@ require("dotenv").config();
 const libraries = ["places"];
 
 const mapContainerStyle = {
-  width: "80vw",
-  height: "90vh",
+  width: "100vw",
+  height: "100vh",
   alignSelf: "center",
-  marginLeft: "10%",
+  //marginLeft: "30%",
 };
 
 const center = {
@@ -41,7 +38,6 @@ const center = {
 };
 
 const options = {
-  styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
 };
@@ -68,76 +64,6 @@ export default function App() {
   if (!isLoaded) return "Loading Maps";
   return (
     <div>
-      <div
-        className="imgDiv"
-        style={{
-          width: "50%",
-          height: "100%",
-        }}
-      >
-        <img src={logo} alt="logo" className="logo"></img>
-      </div>
-
-      <div style={{ textAlign: "center", marginTop: "3rem" }}>
-        <strong
-          style={{
-            fontFamily:
-              "Trebuchet MS, Lucida Sans, Lucida Sans Unicode, Lucida Grande, Arial,sans-serif",
-            fontSize: "x-large",
-          }}
-        >
-          {" "}
-          This map will help you find the nearst children mental health facility
-          to you.
-        </strong>
-        <br />
-        <br />
-        <div className="initP">
-          • To find the nearest facility to you, enter your zipcode with the
-          following format:{" "}
-          <strong style={{ color: "blue" }}>"N2N N2N" </strong>
-          And hit the 'Enter' key on your keyboard OR select an address from the
-          suggestions listed.
-          <hr
-            style={{
-              borderBottomColor: "skyblue",
-              borderBottomWidth: 0.2,
-              width: "20%",
-            }}
-          />
-          <br /> • Or, you can enter an address (could be your home address or
-          an address of your choice), do not forget to hit 'Enter' Or select one
-          of the options listed.
-          <hr
-            style={{
-              borderBottomColor: "skyblue",
-              borderBottomWidth: 0.2,
-              width: "20%",
-            }}
-          />
-          <br />• You can also search by City or/and Province (For example:
-          <strong style={{ color: "blue" }}> London, ON </strong> or{" "}
-          <strong style={{ color: "blue" }}> Ontario</strong>).
-          <hr
-            style={{
-              borderBottomColor: "skyblue",
-              borderBottomWidth: 0.2,
-              width: "20%",
-            }}
-          />
-          <br />• Click the{" "}
-          <strong style={{ color: "red" }}>red markers</strong> on the map to
-          view each facility's information.
-        </div>
-        <div>
-          <hr
-            style={{
-              borderBottomColor: "black",
-              borderBottomWidth: 1,
-            }}
-          />
-        </div>
-      </div>
       <Search panTo={panTo} />
 
       <GoogleMap
@@ -235,6 +161,7 @@ function Search({ panTo }) {
           clearSuggestions();
           try {
             const results = await getGeocode({ address });
+            console.log(results);
             const { lat, lng } = await getLatLng(results[0]);
             panTo({ lat, lng });
           } catch (err) {
